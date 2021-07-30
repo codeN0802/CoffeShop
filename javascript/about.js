823d9ea0f2f7d745da122cef4b93a98ee7d6d4ee
huunghi <nghinguyen08022000@gmail.com>
2021-07-30 21:01:08 +0700

    contact about xong

--- javascript/about.js
+++ javascript/about.js
@@ -1,3 +1,8 @@
+var myheader = document.getElementById('header')
+var mobileMenu = document.getElementById('mobile-menu-nav');
+var heiderheight = myheader.clientHeight
+
+
 var mybutton = document.getElementById("mybtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -16,37 +21,14 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-//
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  
-  slides[slideIndex-1].style.display = "block";  
-  
-}
-var myheader = document.getElementById('header');
 function changecolor(){
   
   var scrollValue = window.scrollY;
   
-  if(scrollValue < 665 ){
+  
+  
+  if(scrollValue < 400){
     myheader.classList.remove('bgColor');
     myheader.classList.add('bgColorx')
   }else{
@@ -55,13 +37,3 @@ function changecolor(){
   }
 }
 window.addEventListener('scroll',changecolor);
-var mobileMenu = document.getElementById('mobile-menu-nav');
-var heiderheight = myheader.clientHeight
-mobileMenu.onclick = function(){
-  var isOpen = myheader.clientHeight === heiderheight;
-  if(isOpen){
-    myheader.style.height = 'auto';
-  }else{
-    myheader.style.height = null;
-  }
-}
